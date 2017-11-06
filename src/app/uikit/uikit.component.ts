19e70453d24973d69ff2fd3bcf623d40e96687ef
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2017-11-06 15:54:37 +0530

    Add test ui component

---
+++ src/app/uikit/uikit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ht-uikit',
+  templateUrl: './uikit.component.html',
+  styleUrls: ['./uikit.component.scss']
+})
+export class UikitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
