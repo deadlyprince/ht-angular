992030333e5009a2bef912b954efe52c528e5b2e
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2017-11-06 15:54:17 +0530

    Add bulma and uikit, remove require uikit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {GroupKeyGuard} from "./guard/group-key.guard";
 import {GroupKeyResolver} from "./guard/group-key-resolver";
+import {UikitComponent} from "./uikit/uikit.component";
 
 const routes: Routes = [
+  // { path: 'ui', component: UikitComponent},
   { path: '', loadChildren: "./test/test.module#TestModule"},
   { path: 'groups', loadChildren: "./groups-test/groups-test.module#GroupsTestModule"},
   { path: 'groups/:id', loadChildren: "./group-test/group-test.module#GroupTestModule"}
