90de35e177b3407d56552fc3cc163b38e52d5c32
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2017-11-30 13:47:26 +0530

    navbar with login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {GroupKeyGuard} from "./guard/group-key.guard";
 import {GroupKeyResolver} from "./guard/group-key-resolver";
+import {ModalComponent} from "./modal/modal.component";
 
 const routes: Routes = [
   // { path: 'ui', component: UikitComponent},
-  { path: '', loadChildren: "./test/test.module#TestModule"},
+  { path: 'users', loadChildren: "./test/test.module#TestModule"},
   { path: 'groups', loadChildren: "./groups-test/groups-test.module#GroupsTestModule"},
-  { path: 'groups/:id', loadChildren: "./group-test/group-test.module#GroupTestModule"}
+  { path: 'groups/:id', loadChildren: "./group-test/group-test.module#GroupTestModule"},
+  { path: '',   redirectTo: '/users', pathMatch: 'full' },
 ];
 
 @NgModule({
