f2e995d0132cc008babfbd1b1133267bb9e1aa08
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2017-12-04 14:00:54 +0530

    account service use ht client

--- src/app/accounts/accounts.service.ts
+++ src/app/accounts/accounts.service.ts
@@ -2,23 +2,26 @@ import { Injectable } from '@angular/core';
 import {BehaviorSubject} from "rxjs/BehaviorSubject";
 import {IAccount, IAccountUser, IMembership, Page} from "ht-models";
 import {HttpClient} from "@angular/common/http";
-import {tap} from "rxjs/operators";
+import {filter, skip, tap} from "rxjs/operators";
 import {StorageService} from "../internal/storage.service";
 import {GetSecretToken, getAuthHeaders} from "ht-data";
 import {Observable} from "rxjs/Observable";
 import {HtClientService} from "../ht/ht-client.service";
-import {HtAccountUsersService} from "../ht/ht-account-users.service";
+import {HtAccountService} from "../ht/ht-account-users.service";
 
 @Injectable()
 export class AccountsService {
-  accountUsers$: BehaviorSubject<null | IAccountUser> = new BehaviorSubject(null);
-  memberships$: BehaviorSubject<null | Page<IMembership>> = new BehaviorSubject(null);
+  accountUsers$: Observable<IAccountUser>;
+  // memberships$: BehaviorSubject<null | Page<IMembership>> = new BehaviorSubject(null);
+  memberships$: Observable<Page<IMembership>>;
   constructor(
     private http: HttpClient,
     private storage: StorageService,
     private client: HtClientService,
-    private accountClient: HtAccountUsersService
+    private accountClient: HtAccountService
   ) {
+    this.accountUsers$ = this.accountClient.accountUser.data$;
+    this.memberships$ = this.accountClient.memberships.data$;
     this.getAccountUser()
   }
   environment: 'production' | 'test' = 'production';
@@ -48,20 +51,36 @@ export class AccountsService {
     const id = this.storage.getUserId();
     if (token && id) {
       this.client.token = token;
+      this.accountClient.setToken(token);
       if (tempToken) this.client.tempToken = tempToken;
-      this.fetchAccountUsers(id, token).subscribe((accountUser) => {
+      // this.fetchAccountUsers(id, token).subscribe((accountUser) => {
+      //   this.setAccountUsers(accountUser)
+      // });
+      this.accountClient.accountUser.setId(id);
+      this.accountClient.accountUser.data$
+        .pipe(
+          filter(data => !!data),
+          skip(1)
+        )
+        .subscribe((accountUser) => {
         this.setAccountUsers(accountUser)
       });
-      this.accountClient.api.membershipsAll(id, {}, {headers: getAuthHeaders(token)}).subscribe((members) => {
-        this.memberships$.next(members)
-      })
+
+      // this.accountClient.memberships.data$
+      //   .pipe(filter(data => !!data))
+      //   .subscribe((mem) => {
+      //   console.log("mem", mem);
+      // })
+      // this.accountClient.api.membershipsAll(id, {}, {headers: getAuthHeaders(token)}).subscribe((members) => {
+      //   this.memberships$.next(members)
+      // })
     }
 
 
   }
 
   setAccountUsers(accountUser: IAccountUser) {
-    this.accountUsers$.next(accountUser);
+    // this.accountUsers$.next(accountUser);
     const account = accountUser.default_account;
     const id = accountUser.id;
     this.setAccount(account);
@@ -73,5 +92,11 @@ export class AccountsService {
     this.client.token = token;
     admin ? this.storage.setToken(token) : this.storage.setTempToken(token)
   };
+
+  setMembership(membership: IMembership) {
+    console.log(membership);
+    this.setAccount(membership.account, false);
+    location.reload()
+  }
 
 }
