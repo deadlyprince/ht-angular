3e48495750dfaeea939e888dc3ca6bae5a81554c
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2017-12-19 12:55:57 +0530

    fix client of users analytics service

--- src/app/users-analytics-list/users-analytics-list.component.ts
+++ src/app/users-analytics-list/users-analytics-list.component.ts
@@ -1,20 +1,35 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {IAnalyticsList} from "../interfaces/analytics-list";
+import {IUserAnalytics} from "ht-models/dist/typings/user";
 
 @Component({
   selector: 'ht-users-analytics-list',
   templateUrl: './users-analytics-list.component.html',
   styleUrls: ['./users-analytics-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UsersAnalyticsListComponent implements OnInit {
+export class UsersAnalyticsListComponent implements OnInit, OnDestroy {
   @Input() listService: IAnalyticsList;
-
+  selectedUser: IUserAnalytics | null = null;
   constructor() {
 
   }
 
   ngOnInit() {
 
+  }
+
+  showUserDetail(row) {
+    this.selectedUser = row.data;
+  }
+
+  closeModal() {
+    this.selectedUser = null;
+  }
+
+  ngOnDestroy() {
+    this.listService.client.destroy();
+    // this.listService.client.list.dataSub.unsubscribe()
   }
 
 }
--- src/app/users-analytics-list/users-analytics-list.module.ts
+++ src/app/users-analytics-list/users-analytics-list.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersAnalyticsListComponent } from './users-analytics-list.component';
 import {DateRangeModule} from "../filters/date-range/date-range.module";
+import {UserTableModule} from "../user-table/user-table.module";
 
 @NgModule({
   imports: [
     CommonModule,
     DateRangeModule,
+    UserTableModule
   ],
   declarations: [UsersAnalyticsListComponent],
   exports: [UsersAnalyticsListComponent]
