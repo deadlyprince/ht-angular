742857e9eb3cbcf3a87c83aff87f7b465f67231f
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2018-01-02 13:50:55 +0530

    use ng-packagr, add public api, add ng-package.json, add externals in config

---
+++ src/public_api.ts
@@ -0,0 +1,109 @@
+export {UserCardModule} from "./app/user-card/user-card.module";
+export {UserCardComponent} from "./app/user-card/user-card.component";
+export {UsersComponent} from "./app/users/users.component";
+export {UsersModule} from "./app/users/users.module";
+export {UsersContainerModule} from "./app/users-container/users-container.module";
+export {UsersContainerComponent} from "./app/users-container/users-container.component";
+export {GroupsModule} from "./app/groups/groups.module";
+export {GroupsComponent} from "./app/groups/groups.component";
+export {GroupsContainerModule} from "./app/groups-container/groups-container.module";
+export {GroupsContainerComponent} from "./app/groups-container/groups-container.component";
+export {GroupsChartContainerModule} from "./app/groups-chart-container/groups-chart-container.module";
+export {GroupsChartContainerComponent} from "./app/groups-chart-container/groups-chart-container.component";
+export {MapModule} from "./app/map/map.module";
+export {MapContainerModule} from "./app/map-container/map-container.module";
+export {MapContainerComponent} from "./app/map-container/map-container.component";
+export {SharedModule} from "./app/shared/shared.module";
+export {PlacelineContainerModule} from "./app/placeline-container/placeline-container.module";
+export {PlacelineContainerComponent} from "./app/placeline-container/placeline-container.component";
+export {PlacelineModule} from "./app/placeline/placeline.module";
+export {PlacelineComponent} from "./app/placeline/placeline.component";
+export {PlacelineMapContainerModule} from "./app/placeline-map-container/placeline-map-container.module";
+export {PlacelineMapContainerComponent} from "./app/placeline-map-container/placeline-map-container.component";
+export {UsersMapContainerModule} from "./app/users-map-container/users-map-container.module";
+export {UsersMapContainerComponent} from "./app/users-map-container/users-map-container.component";
+export { GroupKeyResolver} from "./app/guard/group-key-resolver";
+export { GroupLookupKeyResolver } from "./app/guard/group-lookup-key-resolver";
+export {HtClientService} from "./app/ht/ht-client.service";
+export {HtUsersService} from "./app/ht/ht-users.service";
+export {HtMapService} from "./app/ht/ht-map.service";
+export {HtGroupsService} from "./app/ht/ht-groups.service";
+export { UsersAnalyticsListModule } from "./app/users-analytics-list/users-analytics-list.module"
+export { UsersAnalyticsListComponent } from "./app/users-analytics-list/users-analytics-list.component"
+export { ActionsStatusGraphModule } from "./app/actions-status-graph/actions-status-graph.module"
+export { ActionsStatusGraphComponent } from "./app/actions-status-graph/actions-status-graph.component"
+export { UserTableModule } from "./app/user-table/user-table.module"
+export { UserTableComponent } from "./app/user-table/user-table.component"
+export { AnalyticsContainerModule } from "./app/analytics-container/analytics-container.module"
+export { AnalyticsContainerComponent } from "./app/analytics-container/analytics-container.component"
+export { UsersSummaryChartComponent } from "./app/users-summary-chart/users-summary-chart.component"
+export { UsersSummaryChartModule } from "./app/users-summary-chart/users-summary-chart.module"
+
+export * from "./app/ht/ht.module"
+
+// import {UserCardModule} from "./app/user-card/user-card.module";
+// import {UserCardComponent} from "./app/user-card/user-card.component";
+// import {UsersComponent} from "./app/users/users.component";
+// import {UsersModule} from "./app/users/users.module";
+// import {UsersContainerModule} from "./app/users-container/users-container.module";
+// import {UsersContainerComponent} from "./app/users-container/users-container.component";
+// import {GroupsModule} from "./app/groups/groups.module";
+// import {GroupsComponent} from "./app/groups/groups.component";
+// import {GroupsContainerModule} from "./app/groups-container/groups-container.module";
+// import {GroupsContainerComponent} from "./app/groups-container/groups-container.component";
+// import {GroupsChartContainerModule} from "./app/groups-chart-container/groups-chart-container.module";
+// import {GroupsChartContainerComponent} from "./app/groups-chart-container/groups-chart-container.component";
+// import {MapModule} from "./app/map/map.module";
+// import {MapContainerModule} from "./app/map-container/map-container.module";
+// import {MapContainerComponent} from "./app/map-container/map-container.component";
+// import {SharedModule} from "./app/shared/shared.module";
+// import {PlacelineContainerModule} from "./app/placeline-container/placeline-container.module";
+// import {PlacelineContainerComponent} from "./app/placeline-container/placeline-container.component";
+// import {PlacelineModule} from "./app/placeline/placeline.module";
+// import {PlacelineComponent} from "./app/placeline/placeline.component";
+// import {PlacelineMapContainerModule} from "./app/placeline-map-container/placeline-map-container.module";
+// import {PlacelineMapContainerComponent} from "./app/placeline-map-container/placeline-map-container.component";
+// import {UsersMapContainerModule} from "./app/users-map-container/users-map-container.module";
+// import {UsersMapContainerComponent} from "./app/users-map-container/users-map-container.component";
+// import { GroupKeyResolver} from "./app/guard/group-key-resolver";
+// import { GroupLookupKeyResolver } from "./app/guard/group-lookup-key-resolver";
+// import {HtClientService} from "./app/ht/ht-client.service";
+// import {HtUsersService} from "./app/ht/ht-users.service";
+// import {HtMapService} from "./app/ht/ht-map.service";
+// import {HtGroupsService} from "./app/ht/ht-groups.service";
+//
+// export {
+//   UserCardComponent,
+//   UserCardModule,
+//   UsersComponent,
+//   UsersModule,
+//   UsersContainerComponent,
+//   UsersContainerModule,
+//   GroupsComponent,
+//   GroupsContainerComponent,
+//   GroupsModule,
+//   GroupsContainerModule,
+//   GroupsChartContainerModule,
+//   GroupsChartContainerComponent,
+//   MapContainerComponent,
+//   MapContainerModule,
+//   MapModule,
+//   SharedModule,
+//   PlacelineMapContainerComponent,
+//   PlacelineMapContainerModule,
+//   PlacelineComponent,
+//   PlacelineModule,
+//   PlacelineContainerModule,
+//   PlacelineContainerComponent,
+//   UsersMapContainerModule,
+//   UsersMapContainerComponent,
+//   GroupKeyResolver,
+//   GroupLookupKeyResolver,
+//   HtUsersService,
+//   HtMapService,
+//   HtClientService,
+//   HtGroupsService
+// }
+//
+// export * from "./app/ht/ht.module"
+//
